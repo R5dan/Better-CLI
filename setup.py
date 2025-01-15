10a5e512c59f0a3a09eb7f3ecec5ad098affb43f
R5dan <r5dan@outlook.com>
2025-01-15 21:05:30 +0000

    Setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="bettercli",
+    version="0.0.1",
+    description="A better CLI library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/r5dan/better-cli",
+    author="R5dan",
+    license="Apache",
+    classifiers=[
+        "License :: OSI Approved :: MIT",
+        "Development Status :: 3 - Alpha",
+        #"Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+
+
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    platforms=["any"],
+    keywords="cli, command line, ",
+    packages=find_packages(exclude=["contrib", "docs", "tests", "examples"]),
+    install_requires=[],
+    extras_require={}
+)
