b6e141ed2658abb96b5dff135035c95dbd57a8fa
R5dan <r5dan@outlook.com>
2025-01-17 22:47:57 +0000

    Implement coderabitai suggestions

--- bettercli/decorators.py
+++ bettercli/decorators.py
@@ -8,29 +8,29 @@ T = t.TypeVar("T")
 logger = getLogger("bettercli")
 
 @t.overload
-def pos_option(name: 'str', type_: 'type[T]', default: 'T' = None, *, length: 'int' = 1): ...
+def pos_option(name: 'str', type: 'type[T]', *, default: 'T' = None, length: 'int' = 1): ...
 
 @t.overload
-def pos_option(name: 'str', type_: 'list[t.Optional[type]]' = [], *, default: 'list[t.Optional[t.Any]]' = []): ...
+def pos_option(name: 'str', type: 'list[t.Optional[type]]' = None, *, default: 'list[t.Optional[t.Any]]' = None): ...
 
-def pos_option(name, type_=None, *, default=None, length=1):
+def pos_option(name, type=None, *, default=None, length=1):
     def decorator(cmd:'Command'):
-        logger.debug(f"OPTIONS: {name=} {type_=} {default=} {length=}")
-        cmd.add_positional_option(name, type_, default=default, length=length)
+        logger.debug(f"OPTIONS: {name=} {type=} {default=} {length=}")
+        cmd.add_positional_option(name, type, default=default, length=length)
         return cmd
     return decorator
 
 
 
 @t.overload 
-def kw_option(name: 'str', type_: 'type[T]', *keys: str, default: 'T' = None, length: 'int' = 1, max_occurrences: 'int' = 1): ...
+def kw_option(name: 'str', type: 'type[T]', *keys: str, default: 'T' = None, length: 'int' = 1, max_occurrences: 'int' = 1): ...
 
 @t.overload
-def kw_option(name: 'str', type_: 'list[t.Optional[type]]' = [], *keys: str, default: 'list[t.Optional[t.Any]]' = [], max_occurrences: 'int' = 1): ...
+def kw_option(name: 'str', type: 'list[t.Optional[type]]' = None, *keys: str, default: 'list[t.Optional[t.Any]]' = None, max_occurrences: 'int' = 1): ...
 
-def kw_option(name, type_=None, *keys, default=None, length=2, max_occurrences=1):
+def kw_option(name, type=None, *keys, default=None, length=1, max_occurrences=1):
     def decorator(cmd:'Command'):
         logger.debug("KW ARG")
-        cmd.add_keyword_option(name, type_, default, *keys, length=length, max_occurrences=max_occurrences)
+        cmd.add_keyword_option(name, type, default, *keys, length=length, max_occurrences=max_occurrences)
         return cmd
     return decorator
